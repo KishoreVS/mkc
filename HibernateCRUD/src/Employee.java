66f3bf06655769f657687444b5580bac562edab9
kumbhar <satish.a.kumbhar@capgemini.com>
2019-08-08 17:32:19 +0530

    added jpa

---
+++ HibernateCRUD/src/Employee.java
@@ -0,0 +1,28 @@
+
+public class Employee {
+	int eid;
+	String ename;
+	public int getEid() {
+		return eid;
+	}
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public Employee(int eid, String ename) {
+		super();
+		this.eid = eid;
+		this.ename = ename;
+	}
+	public Employee() {}
+	
+	
+	
+	
+	
+}
